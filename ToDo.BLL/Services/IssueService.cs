2770a916726586955a8a49f9f359152ae239ee7f
Bohdan Bybliv <bohdanbybliv7@gmail.com>
2023-09-11 19:04:01 +0300

    Complete service

--- ToDo.BLL/Services/IssueService.cs
+++ ToDo.BLL/Services/IssueService.cs
@@ -1,9 +1,5 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ToDo.BLL.DTOs;
 using ToDo.BLL.Interfaces;
 using ToDo.DAL;
@@ -21,13 +17,47 @@ namespace ToDo.BLL.Services
             _mapper = mapper;
             _context = context;
         }
-        public async Task Create(IssueDTO issueDTO)
+        public async Task<IEnumerable<IssueDTO>> GetAllAsync()
+        {
+            var issues = _context.Issues.AsNoTracking();
+
+            return await issues.Select(i => _mapper.Map<IssueDTO>(i)).ToListAsync();
+        }
+        public async Task<int> Create(IssueDTO issueDTO)
         {
             var issue = _mapper.Map<Issue>(issueDTO);
 
             await _context.Issues.AddAsync(issue);
 
             await _context.SaveChangesAsync();
+
+            return issue.Id;
+        }
+        public async Task<int> Update(IssueDTO issueDTO)
+        {
+            var issue = await _context.Issues.FindAsync(issueDTO.Id);
+
+            if (issue is null)
+                return 404;
+
+            _mapper.Map(issueDTO, issue);
+
+            await _context.SaveChangesAsync();
+
+            return issue.Id;
+        }
+        public async Task<int> Delete(int id)
+        {
+            var issue = _context.Issues.Find(id);
+
+            if (issue is null)
+                return 404;
+
+            _context.Remove(issue);
+
+            await _context.SaveChangesAsync();
+
+            return id;
         }
     }
 }
